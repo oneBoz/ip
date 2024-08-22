5fc4272c814d0ed594a3b5f010f708d52dfc5ae7
oneBoz <dingyihe@DYHDYHDYH.local>
2024-08-22 15:47:00 +0800

    Add Friday.java

---
+++ src/main/java/Friday.java
@@ -0,0 +1,14 @@
+public class Friday {
+    public static void main(String[] args) {
+
+        String logo2 = """
+        ____________________________________________________________
+        Hello! I'm Friday
+        What can I do for you?
+        ____________________________________________________________
+        Bye. Hope to see you again soon!
+        ____________________________________________________________""";
+        System.out.println(logo2);
+
+    }
+}
