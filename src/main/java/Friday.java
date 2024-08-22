1f7e897dba7ab14eaa6de9bab0326b2709289d01
oneBoz <dingyihe@DYHDYHDYH.local>
2024-08-22 16:17:34 +0800

    Added echo feature on Friday.java

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,14 +1,61 @@
+import java.util.Scanner;
+
+/**
+ * @author Ding Yi He
+ */
 public class Friday {
     public static void main(String[] args) {
 
-        String logo2 = """
+        String intro = """
         ____________________________________________________________
         Hello! I'm Friday
         What can I do for you?
         ____________________________________________________________
+        
+        Commands:
+        list
+        blah
+        bye
+        """;
+
+        String byeMsg = """
+        ____________________________________________________________
         Bye. Hope to see you again soon!
         ____________________________________________________________""";
-        System.out.println(logo2);
+
+        String listMsg = """
+        ____________________________________________________________
+        list
+        ____________________________________________________________""";
+
+        String blah = """
+        ____________________________________________________________
+        blah
+        ____________________________________________________________""";
+
+
+        boolean exited = false;
+        System.out.println(intro);
+        while (!exited) {
+            Scanner scan = new Scanner(System.in);
+            switch (scan.nextLine()) {
+                case "bye":
+                    System.out.println(byeMsg);
+                    exited = true;
+                    break;
+                case "list":
+                    System.out.println(listMsg);
+                    break;
+                case "blah":
+                    System.out.println(blah);
+                    break;
+                default:
+                    System.out.println("Invalid command! Please retry!");
+                    break;
+
+            }
+
+        }
 
     }
 }
