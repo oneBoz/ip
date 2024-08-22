ca2c0ee09c5c1a1cfa5f46cbd4f145fd68c6533d
oneBoz <dingyihe@DYHDYHDYH.local>
2024-08-22 20:02:45 +0800

    Added level 4 features

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -5,62 +5,69 @@ import java.util.Scanner;
  */
 public class Friday {
 
-    private static void addTask(String s, Task[] tasks, int loc) {
-        tasks[loc] = new Task(s);
+    private static void addTask(Task t, Task[] tasks, int loc) {
+        tasks[loc] = t;
         System.out.println(String.format("""
         ____________________________________________________________
-        added: %s
-        ____________________________________________________________""", s));
+         Got it. I've added this task: 
+          %s
+         Now you have %d tasks in the list.""", t, loc + 1));
     }
 
     private static void listTasks(Task[] tasks, int size) {
 
         System.out.println("""
         ____________________________________________________________
-        Here are the tasks in your list: """);
+         Here are the tasks in your list: """);
         for (int i = 0; i < size; i++) {
-            System.out.println(String.format("%d. %s", i + 1, tasks[i]));
+            System.out.println(String.format(" %d. %s", i + 1, tasks[i]));
         }
 
-        System.out.println("""
-        ____________________________________________________________""");
     }
 
     private static void markTask(Task[] tasks, int loc) {
         tasks[loc].markAsDone();
         System.out.println(String.format("""
         ____________________________________________________________
-        Nice! I've marked this task as done:
-          %s
-        ____________________________________________________________""", tasks[loc]));
+         Nice! I've marked this task as done:
+            %s""", tasks[loc]));
     }
 
     private static void unMarkTask(Task[] tasks, int loc) {
         tasks[loc].unmark();
         System.out.println(String.format("""
         ____________________________________________________________
-        OK, I've marked this task as not done yet:
-          %s
-        ____________________________________________________________""", tasks[loc]));
+         OK, I've marked this task as not done yet:
+           %s""", tasks[loc]));
+    }
+
+    private static String getCommand(String input) {
+        return input.split(" ")[0];
+    }
+
+    private static String getDesc(String input) {
+        return input.replace(getCommand(input) + " ", "");
     }
 
     public static void main(String[] args) {
 
         String intro = """
         ____________________________________________________________
-        Hello! I'm Friday
-        What can I do for you?
+         Hello! I'm Friday
+         What can I do for you?
         ____________________________________________________________
         
-        Commands:
-        list
-        bye
-        """;
+         Commands:
+         list
+         bye
+         todo
+         deadline
+         event
+        ____________________________________________________________""";
 
         String byeMsg = """
         ____________________________________________________________
-        Bye. Hope to see you again soon!
-        ____________________________________________________________""";
+        Bye. Hope to see you again soon!""";
 
 
 
@@ -72,8 +79,9 @@ public class Friday {
         while (!exited) {
             Scanner scan = new Scanner(System.in);
             String input = scan.nextLine();
-            String[] command = input.split(" ");
-            switch (command[0]) {
+            String command = getCommand(input);
+            String desc = getDesc(input);
+            switch (command) {
                 case "bye":
                     System.out.println(byeMsg);
                     exited = true;
@@ -84,19 +92,38 @@ public class Friday {
                     break;
 
                 case "mark":
-                    markTask(tasks, Integer.valueOf(command[1]) - 1);
+                    markTask(tasks, Integer.valueOf(desc) - 1);
                     break;
 
                 case "unmark":
-                    unMarkTask(tasks, Integer.valueOf(command[1]) - 1);
+                    unMarkTask(tasks, Integer.valueOf(desc) - 1);
                     break;
 
-                default:
-                    addTask(input, tasks, loc);
+                case "deadline":
+                    String[] dd = desc.split(" /by ");
+                    addTask(new Deadline(dd[0], dd[1]), tasks, loc);
                     loc++;
                     break;
 
+                case "todo":
+                    addTask(new Todo(desc), tasks, loc);
+                    loc++;
+                    break;
+
+                case "event":
+                    String[] e = desc.split(" /from ");
+                    String[] timing = e[1].split(" /to ");
+                    addTask(new Event(e[0], timing[0], timing[1]), tasks, loc);
+                    loc++;
+                    break;
+
+                default:
+                    System.out.println("Please enter a valid command!");
+                    break;
+
             }
+            System.out.println("""
+                    ____________________________________________________________""");
 
         }
 
