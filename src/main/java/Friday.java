1f7b736ca5bde132ce9e869f149535af97252e0b
oneBoz <dingyihe@DYHDYHDYH.local>
2024-08-22 19:23:25 +0800

    Added level 3 features

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,28 +1,47 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
 /**
  * @author Ding Yi He
  */
 public class Friday {
 
-    private static void addTask(String s, String[] tasks, int loc) {
-        tasks[loc] = s;
+    private static void addTask(String s, Task[] tasks, int loc) {
+        tasks[loc] = new Task(s);
         System.out.println(String.format("""
         ____________________________________________________________
         added: %s
         ____________________________________________________________""", s));
     }
 
-    private static void listTasks(String[] tasks, int size) {
+    private static void listTasks(Task[] tasks, int size) {
 
         System.out.println("""
-        ____________________________________________________________""");
+        ____________________________________________________________
+        Here are the tasks in your list: """);
         for (int i = 0; i < size; i++) {
-            System.out.println(String.format("%d. %s", i, tasks[i]));
+            System.out.println(String.format("%d. %s", i + 1, tasks[i]));
         }
 
         System.out.println("""
         ____________________________________________________________""");
+    }
+
+    private static void markTask(Task[] tasks, int loc) {
+        tasks[loc].markAsDone();
+        System.out.println(String.format("""
+        ____________________________________________________________
+        Nice! I've marked this task as done:
+          %s
+        ____________________________________________________________""", tasks[loc]));
+    }
+
+    private static void unMarkTask(Task[] tasks, int loc) {
+        tasks[loc].unmark();
+        System.out.println(String.format("""
+        ____________________________________________________________
+        OK, I've marked this task as not done yet:
+          %s
+        ____________________________________________________________""", tasks[loc]));
     }
 
     public static void main(String[] args) {
@@ -47,13 +66,14 @@ public class Friday {
 
 
         boolean exited = false;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int loc = 0;
         System.out.println(intro);
         while (!exited) {
             Scanner scan = new Scanner(System.in);
             String input = scan.nextLine();
-            switch (input) {
+            String[] command = input.split(" ");
+            switch (command[0]) {
                 case "bye":
                     System.out.println(byeMsg);
                     exited = true;
@@ -61,6 +81,14 @@ public class Friday {
 
                 case "list":
                     listTasks(tasks, loc);
+                    break;
+
+                case "mark":
+                    markTask(tasks, Integer.valueOf(command[1]) - 1);
+                    break;
+
+                case "unmark":
+                    unMarkTask(tasks, Integer.valueOf(command[1]) - 1);
                     break;
 
                 default:
